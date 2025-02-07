3cdd206e51dd8b31f9ae8f70be2acbae637c453d
benjaehyun <139726967+benjaehyun@users.noreply.github.com>
2025-02-07 13:34:11 -0800

    fix indentation setting and added auth provider placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,26 @@ import Layout from './components/shared/Layout';
 
 function App() {
   return (
+    // Context provider for auth data 
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<Layout />}>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <Search />
-                </ProtectedRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </Router>
+		<Router>
+			<Routes>
+				{/* Route definitions */}
+				<Route path="/login" element={<Login />} />
+				{/* Parent component for pages handling ui layout */}
+				<Route element={<Layout />}>
+					<Route
+					path="/"
+					element={
+						// Protected route wrapper ensuring auth status 
+						<ProtectedRoute>
+						<Search />
+						</ProtectedRoute>
+					}
+					/>
+				</Route>
+			</Routes>
+		</Router>
     </AuthProvider>
   );
 }
---
+++ src/components/auth/AuthProvider.jsx
@@ -0,0 +1,9 @@
+import React, {createContext, useContext, useState} from 'react';
+import axios from 'axios'; 
+
+export const AuthProvider = () => {
+    return (
+        <div>AuthProvider</div>
+    )
+    
+}
