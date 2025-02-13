69925c2401cad40523d421b5c80dd5d52abd3d9f
benjaehyun <139726967+benjaehyun@users.noreply.github.com>
2025-02-12 20:56:32 -0800

    initial layout component with toolbar skeleton

---
+++ src/components/ui/Layout.jsx
@@ -0,0 +1,26 @@
+import { Outlet } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { useAuth } from "../auth/AuthProvider";
+
+const Layout = () => {
+    const { logout } = useAuth()
+
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{flexGrow:1}} >
+                        Fetch Frontend
+                    </Typography>
+                    <Button color="inherit" onClick={logout}>
+                        Logout
+                    </Button>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+
+}
